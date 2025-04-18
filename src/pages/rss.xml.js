18588fb900d97c6f0ffa84ffaa5ab6abe0411db5
Christian Decker <50999401+cmdecker95@users.noreply.github.com>
2025-04-18 08:35:14 -0400

    Format with double quotes (#1)
    Because I'm needlessly pedantic about double quotes

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,9 +1,9 @@
-import rss from '@astrojs/rss';
-import { getCollection } from 'astro:content';
-import { SITE_DESCRIPTION, SITE_TITLE } from '../consts';
+import rss from "@astrojs/rss";
+import { getCollection } from "astro:content";
+import { SITE_DESCRIPTION, SITE_TITLE } from "../consts";
 
 export async function GET(context) {
-  const docs = await getCollection('docs');
+  const docs = await getCollection("docs");
   return rss({
     title: SITE_TITLE,
     description: SITE_DESCRIPTION,
